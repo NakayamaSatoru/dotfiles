52d78267b5ea79a3e89ac6857b3997c58eef9339
satorunakayama <sato@localhost.localdomain>
2017-11-15 05:21:16 +0900

    [fix]きちんとリンクが貼れるように修正

--- setlink.sh
+++ setlink.sh
@@ -1,3 +1,6 @@
-ln -s ~/.dotfile/.vim/.vimrc ~/.vimrc
+unlink ~/.vimrc
+unlink ~/.bashrc
+unlink ~/.bash_profile
+ln -s ~/.dotfile/.vim/.vimrc_cp ~/.vimrc
 ln -s ~/.dotfile/.bashrc ~/.bashrc
 ln -s ~/.dotfile/.bash_profile ~/.bash_profile
