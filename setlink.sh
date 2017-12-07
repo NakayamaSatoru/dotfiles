3e4bf249f0a606c654e1d8674199d2375a85720e
satorunakayama <sato@localhost.localdomain>
2017-12-08 07:16:52 +0900

    [update]設定を変えた

--- setlink.sh
+++ setlink.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+mv ~/.vim ~/.vim_cp
 mv ~/.vimrc ~/.vimrc_cp
 mv ~/.bashrc ~/.bashrc_cp
 mv ~/.bash_profile ~/.bash_profile_cp
+ln -s ~/.dotfile/.vim ~/.vim
 ln -s ~/.dotfile/.vim/.vimrc_cp ~/.vimrc
 ln -s ~/.dotfile/.bashrc ~/.bashrc
 ln -s ~/.dotfile/.bash_profile ~/.bash_profile
