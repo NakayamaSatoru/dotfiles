1ba1d1ed2cb4372ed169e35c177bc098147dbfe8
satorunakayama <n.sato@localhost.localdomain>
2018-01-22 19:50:52 +0900

    いらない文を消した

--- setinstall.sh
+++ setinstall.sh
@@ -3,6 +3,4 @@ sudo dnf install -y boost-devel clang cmake eigen3 libtool make ninja-build prot
 URL=https://downloads.vivaldi.com/stable
 sudo dnf install -y ${URL}/vivaldi-stable-1.11.917.39-1.x86_64.rpm
 git clone https://github.com/Shougo/neobundle.vim ~/.dotfile/.vim/bundle/neobundle.vim
-cd ~/.dotfile/.vim/bundle/vimproc
-make
 cd ~/
